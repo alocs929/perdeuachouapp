66bff4c756f7a0c556f9545e9323370fba2c1f6b
JorderGomes <jordergomes@hotmail.com>
2020-09-25 15:55:45 -0300

    configuração de campos

--- src/pages/CadastroObjeto/index.tsx
+++ src/pages/CadastroObjeto/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
-import {Container, Titulo} from './styles';
-import {Text } from "react-native";
+import {Container, Titulo, CampoTexto, Label, ButtonSave, ButtonSaveText} from './styles';
+import {Text, TextInput } from "react-native";
 
 function CadastroObjetos () {
     const navigation = useNavigation();
@@ -11,6 +11,23 @@ function CadastroObjetos () {
             <Titulo>
                 Cadastrar Pertence
             </Titulo>
+            <Label>Data</Label>
+            <CampoTexto placeholder="Data em que o pertence foi encontrado" />
+
+            <Label>Categoria</Label>
+            <CampoTexto placeholder="Tipo de pertence" />
+
+            <Label>Descrição</Label>
+            <CampoTexto placeholder="Descreva o pertence" />
+
+            <Label>Localização</Label>
+            <CampoTexto placeholder="Possiveis locais onde foi perdido / encontrado" />
+
+            <ButtonSave>
+                <ButtonSaveText>
+                    Finalizar Cadastro
+                </ButtonSaveText>
+            </ButtonSave>
         </Container>
     );
 }
--- src/pages/CadastroObjeto/styles.ts
+++ src/pages/CadastroObjeto/styles.ts
@@ -14,4 +14,36 @@ export const Titulo = styled.Text`
   font-size: 18px;
   width: 100%;
   text-align: center;
+  margin-bottom: 5%;
+`;
+
+export const Label = styled.Text`
+    color: #121212;
+    font-size: 14px;
+    width: 90%;
+`;
+
+export const CampoTexto = styled.TextInput`
+    width: 90%;
+    border: solid 1px #121212;
+    font-size: 16px;
+    padding: 4px;
+    border-radius: 4px;
+    margin-bottom: 4px;
+`;
+
+export const ButtonSave = styled.TouchableOpacity`
+    
+    margin-top: 10%;
+    width: 90%;
+    background-color:  #ff9000;
+    padding: 8px;
+    border-radius: 4px;
+`;
+
+export const ButtonSaveText = styled.Text`
+    width: 100%;
+    color:  #fff;
+    text-align: center;
+    font-size: 14px;
 `;
