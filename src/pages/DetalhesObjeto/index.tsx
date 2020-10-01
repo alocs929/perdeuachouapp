44f531b799c10d8780f685238458f7db4d41f8e5
JorderGomes <jordergomes@hotmail.com>
2020-10-01 13:17:54 -0300

    configuração tela detalhes de objeto

--- src/pages/DetalhesObjeto/index.tsx
+++ src/pages/DetalhesObjeto/index.tsx
@@ -1,14 +1,69 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import PageHeader from '../../components/PageHeader';
+
+import {
+    Container,
+    Footer,
+    EditButton,
+    EditButtonText,
+    BodyObjeto,
+    Imagem,
+    Lista,
+    ListaItem,
+    Topico,
+    Descricao
+} from './styles';
 
 function DetalhesObjeto (){
     const navigation = useNavigation();
     return (
-        <View>
+        <Container>
             <PageHeader title="Detalhes" />
-        </View>
+            <BodyObjeto>
+                {/* <ImgCard></ImgCard> */}
+                <Imagem></Imagem>
+                <Lista>
+                    <ListaItem>
+                        <Topico>Data: </Topico>
+                        <Descricao>30/07/2020</Descricao>
+                    </ListaItem>
+                    <ListaItem>
+                        <Topico>Categoria: </Topico>
+                        <Descricao>
+                            Capacete de motocicleta
+                        </Descricao>
+                    </ListaItem>
+                    <ListaItem>
+                        <Topico>Descrição: </Topico>
+                        <Descricao>
+                            Branco, viseira transparente, adesivo unicornio, 
+                            modelo esportivo, marca IBF
+                        </Descricao>
+                    </ListaItem>
+                    <ListaItem>
+                        <Topico>Localização: </Topico>
+                        <Descricao>
+                            Encontrado no RU da UFC
+                        </Descricao>
+                    </ListaItem>
+                    <ListaItem>
+                        <Topico>Status: </Topico>
+                        <Descricao>
+                            Registrado, em busca do proprietario
+                        </Descricao>
+                    </ListaItem>
+                </Lista>
+            </BodyObjeto>
+            <Footer>
+                <EditButton>
+                    <EditButtonText>
+                        Editar
+                    </EditButtonText>
+                </EditButton>
+            </Footer>
+        </Container>
     );
 }
 
---
+++ src/pages/DetalhesObjeto/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components/native';
+
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  /* justify-content: center; */
+  padding-top: 10%;
+  
+`;
+
+export const BodyObjeto = styled.View`
+    width: 100%;
+    padding: 5%;
+    align-items: center;
+`;
+
+export const Imagem = styled.View`
+    background-color: #ccc;
+    width: 100%;
+    height: 200px;
+`;
+
+export const Lista = styled.View`
+    width: 100%;
+    flex-direction: column;
+`;
+
+export const ListaItem = styled.View`
+    width: 100%;
+    flex-direction: row;
+`;
+
+export const Topico = styled.Text`
+    font-weight: bold;
+`;
+
+export const Descricao = styled.Text`
+    flex: 1;
+`;
+
+export const Footer = styled.View`
+    border: 1px solid #ff6b6b;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 15%;
+    position: absolute;
+    bottom: 0;
+`;
+
+export const EditButton = styled.TouchableOpacity`
+  background-color: #ff9000;
+  width: 50%;
+  padding: 8px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 16px;
+  
+`;
+
+export const EditButtonText = styled.Text`
+  text-align: center;
+  align-items: center;
+  color: #fff;
+  font-size: 18px;
+  width: 100%;
+  
+`;
+
